bf2fe56228f8b5836f52e2bca86aff7b0ada0a70
Prabhat Tuladhar <PRABHATTULADHAR@GMAIL.COM>
2023-11-21 22:36:44 -0500

    ASL Alphabet Detection

---
+++ ScikitLearnAlphabetDetection/RightHandTrainingData.py
@@ -0,0 +1,120 @@
+#make sure you install opencv2, mediapipe and scikit-learn
+
+import mediapipe as mp #import mediapipe
+import cv2 #import opencv2
+import numpy as np
+import csv
+import os
+
+mp_drawing = mp.solutions.drawing_utils #mediapipe drawing helpers
+mp_holistic = mp.solutions.holistic #mediapipe
+
+#Get feed from webcam
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      continue
+
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    #make detections
+    results = holistic.process(image)
+    # Draw landmark annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    #FACE TESSALATION(mesh in face)
+    '''mp_drawing.draw_landmarks(
+        image,results.face_landmarks,mp_holistic.FACEMESH_TESSELATION,#MODEL
+        mp_drawing.DrawingSpec(color=(77, 89, 7), thickness=1, circle_radius=1),#specks
+        mp_drawing.DrawingSpec(color=(82, 39, 24), thickness=1, circle_radius=10)#connections
+     )'''
+    #FACE CONTOURS
+    '''mp_drawing.draw_landmarks(
+       image,
+        results.face_landmarks,
+        mp_holistic.FACEMESH_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 0, 0), thickness=2, circle_radius=2)
+    )'''
+    #RIGHT HAND
+    mp_drawing.draw_landmarks(
+        image,
+        results.right_hand_landmarks,
+        mp_holistic.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(77, 89, 7), thickness=2, circle_radius=5),  # specks
+        mp_drawing.DrawingSpec(color=(237, 248, 255), thickness=2, circle_radius=2)  # skeleton
+    )
+    #LEFT HAND
+    mp_drawing.draw_landmarks(
+        image,
+        results.left_hand_landmarks,
+        mp_holistic.HAND_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(77, 89, 7), thickness=2, circle_radius=5),  # specks
+        mp_drawing.DrawingSpec(color=(237, 248, 255), thickness=2, circle_radius=2)  # skeleton
+    )
+    #POSE
+    '''mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_holistic.POSE_CONNECTIONS,
+        mp_drawing.DrawingSpec(color=(77, 89, 7), thickness=2, circle_radius=5),#specks
+        mp_drawing.DrawingSpec(color=(237, 248, 255), thickness=2, circle_radius=2)#skeleton
+    )'''
+
+    '''class_name = "Y"
+    try:
+        #extract right hand landmarks
+        right_hand=results.right_hand_landmarks.landmark
+        right_hand_row=list(
+           np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in right_hand]).flatten())
+
+    #    #extract left hand landmarks
+    #    left_hand = results.left_hand_landmarks.landmark
+    #    left_hand_row = list(
+    #        np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in left_hand]).flatten())
+        #adding them together
+        row = right_hand_row
+        row.insert(0, class_name)
+
+        #print(len(row))#168
+        with open('right_coords.csv', mode='a', newline='') as f:
+            csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(row)
+    except:
+        pass'''
+    '''
+    #FIND NUMBER OF COORDINATES
+    try:
+        num_coords = len(results.right_hand_landmarks.landmark)
+        print(num_coords)#42 coordinates (21+21 =42)
+    except:
+        pass'''
+
+    #SETUP CSV data namining convetion
+    '''landmarks = ['class']
+    for val in range(1, 21+1):
+        landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+
+    with open('right_coords.csv', mode='w', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(landmarks)'''
+
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+      break
+cap.release()
+
+
+
+
+
+
+
---
+++ ScikitLearnAlphabetDetection/TrainingRight_Hand.py
@@ -0,0 +1,52 @@
+#make sure you install opencv2, mediapipe and scikit-learn
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df=pd.read_csv('right_coords.csv')
+
+#print(df[df['class']=="Happy"])#to filter data in pandas
+
+X=df.drop('class',axis=1)#features
+Y=df['class']#target value
+
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.3,random_state=1234)
+
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler #standardizes the data so one feature over-shadows other
+
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+pipelines = {
+    'lr':make_pipeline(StandardScaler(), LogisticRegression()),
+    'rc':make_pipeline(StandardScaler(), RidgeClassifier()),
+    'rf':make_pipeline(StandardScaler(), RandomForestClassifier()),
+    'gb':make_pipeline(StandardScaler(), GradientBoostingClassifier())
+}
+#print(list(pipelines.values())[2])
+
+fit_models={} #blank dictionary
+for algorithm,pipeline in pipelines.items():
+    model=pipeline.fit(X_train.values, Y_train.values)#added Xtrain.values
+    fit_models[algorithm]=model
+print("Status:Training has Completed")
+
+#print(fit_models)
+#print(fit_models['rf'].predict(X_test))
+
+from sklearn.metrics import accuracy_score# accuracy metrics
+
+for algorithm, model in fit_models.items():
+    #predictions
+    Y_hat=model.predict(X_test.values)#added .values
+    print(algorithm,accuracy_score(Y_test,Y_hat))
+
+#check
+#print(list(fit_models['rf'].predict(X_test)))
+#print(list(Y_test))
+
+import pickle #save training model
+with open('RightHandTraining.pkl', 'wb') as f:#
+    pickle.dump(fit_models['rf'],f)
+#Random Forest accuracy was 97.5
